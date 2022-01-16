95c1e4298818fba06e9402766f5dc6b663f85b73
Tingting Duan <duan.tingting@yahoo.com>
2022-01-15 22:47:25 -0500

    chapter 7 revisited

--- levelup/urls.py
+++ levelup/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from levelupapi.views import register_user, login_user, GameTypeView
+from levelupapi.views import register_user, login_user, GameTypeView, GameView, EventView
 
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'game')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
